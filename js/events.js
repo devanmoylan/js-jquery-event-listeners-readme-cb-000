584c98164985543beedf8e32613fd09647799be2
Devan <devan.moylan@backyardlion.com>
2017-08-24 18:36:53 -0600

    Done.

--- js/events.js
+++ js/events.js
@@ -1,19 +1,38 @@
 //defined functions here
 
 function getIt() {
-    $('p').on('click', function() {
+    $('p').on('click', function () {
         alert("Hey!")
     })
 }
 
 function frameIt() {
-    $('img').on('load', function() {
+    $('img').on('load', function () {
         $('img').addClass("tasty")
     })
 }
 
-$(document).ready(function(){
+function submitIt() {
+    $("form").on("submit", function () {
+        alert('Your form is going to be submitted now.');
+        return;
+    });
+};
 
-    getIt()
-    frameIt()
+
+function pressIt() {
+    $(document).on('keydown', function (e) {
+        if (e == 71) {
+            alert("" +
+                "g was pressed!")
+        }
+    })
+}
+
+
+$(document).ready(function () {
+    getIt();
+    frameIt();
+    submitIt();
+    pressIt();
 });
